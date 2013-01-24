11c8fa6be60bdc853969153a6c7a9c8291eb0309
Fauntleroy <the.kaempfer@gmail.com>
2013-01-24 10:52:39 -0800

    configure timbot for new campfire room

--- timbot.js
+++ timbot.js
@@ -24,158 +24,192 @@ var Timbot = function( params ){
 	var bot = this;
 	var room;
 	var room_id = params.room_id;
-	var insults = params.insults;
-	var wins = params.wins;
+	this.insults = params.insults;
+	this.wins = params.wins;
 	
-	// Join Campfire room and listen for messages
-	campfire.room( room_id, function( selected_room ){
+	campfire.me( function( self ){
 		
-		room = selected_room;
-		
-		room.join( function(){
-			
-			console.log('TIMBOT is listening...');
-			
-			room.listen( function( message ){
+		bot.self = self;
 
-				var command = params.command.exec( message.body );
+		// Join Campfire room and listen for messages
+		campfire.room( room_id, function( selected_room ){
+			
+			bot.room = room = selected_room;
+			
+			room.join( function(){
 				
-				if( command ){
-					
-					if( command[1] === 'LET\'S DO THIS' )
-						bot.listUsers();
-						
-					else if( command[1] === 'LET\'S DO THIS, DANIEL STYLE' )
-						bot.listUsers({
-							last: 'Daniel O\'Shea'
-						});
-						
-					else if( command[1] === 'INSULT SOMEONE' )
-						bot.insult();
-					
-					else if( command[1] === 'EMOTICON SOMEONE' )
-						bot.emoticon();
+				console.log('TIMBOT is listening...');
+				
+				bot.say('http://freewebs.com/chloemew4ever2/19.JPG');
+				bot.say('TIMBOT, REPORTING FOR DUTY');
 
-					else if( command[1] === 'GTFO' )
-						bot.leave();
+				room.listen( function( message ){
+
+					var command = params.command.exec( message.body );
+
+					if( command ){
+						switch( command[1] ){
+						
+						case 'LET\'S DO THIS':
+							bot.listUsers();
+							break;
+
+						case 'LET\'S DO THIS, TIM STYLE':
+							bot.listUsers({
+								last: 'Timothy Kempf'
+							});
+							break;
+
+						case 'INSULT SOMEONE':
+							bot.insult();
+							break;
+
+						case 'EMOTICON SOMEONE':
+							bot.emoticon();
+							break;
+
+						case 'GTFO':
+							bot.leave();
+							break;
+
+						case 'THAT\'S ALL FOR TODAY':
+							bot.thatsAll();
+							break;
+
+						default:
+							bot.say('what.');
+							break;
+
+						}
+					}
 					
-					else if( command[1] === 'THAT\'S ALL FOR TODAY' )
-						bot.thatsAll();
-					
-					else
-						bot.say('what.');
-					
-				}
+				});
 				
 			});
-			
+
 		});
 		
 	});
 	
-	// Utility function for getting user list and randomizing it
-	var getUsernames = function( callback ){
-		
-		room.users( function( users ){
-			
-			var usernames = _.pluck( users, 'name' );
-			usernames = _.without( usernames, 'TIM BOT' );
-			usernames = _.shuffle( usernames );
-			
-			callback( usernames );
-			
-		});
-		
-	};
+};
+
+// Utility function for getting user list and randomizing it
+Timbot.prototype.getUsernames = function( callback ){
 	
-	// Sends a list of users, comma separated
-	// 'last' parameter moves specified username to the end of the list
-	this.listUsers = function( options ){
+	var bot = this;
+	var room = bot.room;
+
+	room.users( function( users ){
 		
-		var options = options || {};
+		var usernames = _.pluck( users, 'name' );
+		usernames = _.without( usernames, bot.self.name );
+		usernames = _.shuffle( usernames );
 		
-		getUsernames( function( usernames ){
-			
-			if( options.last ){
-				var usernames = _.sortBy( usernames, function( username ){
-					return username === options.last;
-				});
-			}
-			var username = usernames[0];
-			var win_message = _.shuffle( wins )[0];
-			var usernames_list = usernames.join(', ');
-			
-			room.speak( usernames_list );
-			room.speak( username + win_message );
-			room.update({
-				topic: usernames_list
+		callback( usernames );
+		
+	});
+	
+};
+
+// Sends a list of users, comma separated
+// 'last' parameter moves specified username to the end of the list
+Timbot.prototype.listUsers = function( options ){
+	
+	var options = options || {};
+	var bot = this;
+	var room = this.room;
+
+	this.getUsernames( function( usernames ){
+		
+		if( options.last ){
+			var usernames = _.sortBy( usernames, function( username ){
+				return username === options.last;
 			});
-			
-		});
+		}
+		var username = usernames[0];
+		var win_message = _.shuffle( bot.wins )[0];
+		var usernames_list = usernames.join(', ');
 		
-	};
-	
-	// Insults a user, randomly by default
-	// 'username' defines a user to insult
-	this.insult = function( username ){
-		
-		getUsernames( function( usernames ){
-			
-			var insult = _.shuffle( insults )[0];
-			
-			if( !username )
-				username = usernames[0];
-			
-			room.speak( username + insult );
-			
-		});
-		
-	};
-
-	// Sends a user an emoticon
-	this.emoticon = function( username ){
-
-		getUsernames( function( usernames ){
-
-			if( !username )
-				username = usernames[0];
-
-			var emoticon = _( emoticons ).random();
-
-			room.speak( username +': :'+ emoticon +':' );
-
-		});
-
-	};
-	
-	// Says something
-	this.say = function( message ){
-		
-		room.speak( message );
-		
-	};
-	
-	// Ends the sync up
-	this.thatsAll = function (){
-		
+		room.speak( usernames_list );
+		room.speak( username + win_message );
 		room.update({
-			topic: ''
+			topic: usernames_list
 		});
 		
-		this.leave();
-		
-	};
+	});
 	
-	// Leaves the room
-	this.leave = function(){
+};
 
-		room.stopListening();
-		room.leave();
-		
-		console.log('TIMBOT has self terminated.');
-
-	};
+// Insults a user, randomly by default
+// 'username' defines a user to insult
+Timbot.prototype.insult = function( username ){
 	
+	var bot = this;
+	var room = this.room;
+
+	this.getUsernames( function( usernames ){
+		
+		var insult = _.shuffle( bot.insults )[0];
+		
+		if( !username )
+			username = usernames[0];
+		
+		room.speak( username + insult );
+		
+	});
+	
+};
+
+// Sends a user an emoticon
+Timbot.prototype.emoticon = function( username ){
+
+	var room = this.room;
+
+	this.getUsernames( function( usernames ){
+
+		if( !username )
+			username = usernames[0];
+
+		var emoticon = _( emoticons ).random();
+
+		room.speak( username +': :'+ emoticon +':' );
+
+	});
+
+};
+
+// Says something
+Timbot.prototype.say = function( message ){
+	
+	var room = this.room;
+
+	room.speak( message );
+	
+};
+
+// Ends the sync up
+Timbot.prototype.thatsAll = function (){
+	
+	var room = this.room;
+
+	room.update({
+		topic: ''
+	});
+	
+	this.leave();
+	
+};
+
+// Leaves the room
+Timbot.prototype.leave = function(){
+
+	var room = this.room;
+
+	room.stopListening();
+	room.leave();
+	
+	console.log('TIMBOT has self terminated.');
+
 };
 
 var timbot = new Timbot({
@@ -183,7 +217,7 @@ var timbot = new Timbot({
 	token: config.token,				// your Campfire token, find it in your Campfire user profile
 	account: config.account,			// 'sparkartengineering', this is the room URL's subdomain usually
 	room_id: config.room_id,					// ID number of the room you want to join
-	command: /TIM\sBOT\:\s(.*)/,		// regex that matches your command flag. keep matching parens at end
+	command: /Tim\sBot\:\s(.*)/,		// regex that matches your command flag. keep matching parens at end
 	insults: config.insults,			// array of strings
 	wins: config.wins					// array of strings
 });
